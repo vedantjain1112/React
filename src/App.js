f8efee648ab9d137ce95c0470c5c8e868c3ee7c8
VEDANT JAIN <jainvedant1211@gmail.com>
2023-10-10 19:14:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Form from "./Form";
 import Carousel from "./Carousel";
+import Calculator from "./Calculator";
 
 function App() {
   return (
     <>
       <Form />
       <Carousel />
+      <Calculator />
     </>
   );
 }
